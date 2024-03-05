2d41d26e10cd8a0416c3864d2b49b8ab1654e0bd
arslankhan21 <arslan.ahmad@narsunstudios.com>
2024-03-05 18:35:55 +0500

    images changed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,18 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:arslankhanVision@gmail.com">arslankhanVision@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/m-a-a-k/">linkedin.com/in/m-a-a-k/</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://github.com/HerculesVirus">github.com/HerculesVirus</a>
         </li>
       </ul>
     </footer>
